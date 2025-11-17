f9228b09c4d5c006d56e5337146da4997d3446a0
BhagyashreeSherkhanee <bhagyashreesherkhanee@gmail.com>
2025-11-18 02:08:25 +0530

    Add files via upload

---
+++ Assignments/Assignment_7/program1_7.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int i = 0;
+    for(i = 1; i <= iNo; i++)
+    {
+        printf("$\t*\t");
+    }
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
